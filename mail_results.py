b9d7f9fc7f651462baabdcb3215624f8995f8db5
Telvis Calhoun <telvis07@gmail.com>
2012-03-21 20:58:59 -0400

    support latin-1 and utf-8

--- mail_results.py
+++ mail_results.py
@@ -3,23 +3,36 @@ import smtplib,sys,os
 from email.mime.text import MIMEText
 from datetime import datetime
 import ConfigParser
-#TODO cont: load json results
 
-def send_email(config, to, subj, msg):
+def send_email(config, to, subj, msg, dryrun=False):
+    
+    # Config and Credentials (if needed)  
     me = config.get('email','mailfrom')
-    msg = MIMEText(msg)
-    msg['Subject'] = subj
-    msg['From'] = me
-    msg['To'] = to
-
-    # Credentials (if needed)  
     username = config.get('email','username')
     password = config.get('email','password')
     server = config.get('email','server')
     port = config.getint('email','port')
-      
+
+    # We must choose the body charset manually
+    for body_charset in 'US-ASCII', 'ISO-8859-1', 'UTF-8':
+        try:
+            msg.encode(body_charset)
+        except UnicodeError:
+            pass
+        else:
+            break
+          
     # The actual mail send  
     try:
+        msg = MIMEText(msg.encode(body_charset), 'plain', body_charset)
+        msg['Subject'] = subj
+        msg['From'] = me
+        msg['To'] = to
+
+        if dryrun:
+            print "Dry-run email:",msg.as_string()
+            return
+
         server = smtplib.SMTP(server, port=port)
         server.login(username,password)  
         server.sendmail(me, to, msg.as_string())  
