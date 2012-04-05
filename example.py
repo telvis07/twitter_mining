a16cb9f905ef550693259e189e28214a21a3672e
Telvis Calhoun <telvis07@gmail.com>
2012-04-04 21:15:05 -0400

    location filtering example

--- example.py
+++ example.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 import tweepy
 import ConfigParser
-import os
+import os, sys
 
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
         print "screen_name='%s' tweet='%s'"%(status.author.screen_name, status.text)
 
-def login():
-    config = ConfigParser.RawConfigParser()
-    fn = 'twitter_mining.cfg'
-    config.read(fn)
+def login(config):
+    """Tweepy oauth dance
+    The config file should contain:
 
+    [auth]
+    CONSUMER_KEY = ...
+    CONSUMER_SECRET = ...
+    ACCESS_TOKEN = ...
+    ACCESS_TOKEN_SECRET = ...
+    """
     CONSUMER_KEY = config.get('auth','CONSUMER_KEY')
     CONSUMER_SECRET = config.get('auth','CONSUMER_SECRET')
     ACCESS_TOKEN = config.get('auth','ACCESS_TOKEN')
@@ -22,9 +27,13 @@ def login():
     return auth
 
 
+fn=sys.argv[1]
+config = ConfigParser.RawConfigParser()
+config.read(fn)
 try:
-    auth = login()
+    auth = login(config)
     streaming_api = tweepy.streaming.Stream(auth, Listener(), timeout=60)
-    streaming_api.filter(follow=None, track=['technical','elvis'])
+    # San Francisco area.
+    streaming_api.filter(follow=None, locations=[-122.75,36.8,-121.75,37.8]) 
 except KeyboardInterrupt:
     print "got keyboardinterrupt"
